a0a9ca1f6ed2c140abf0a0f972292d3d1fa23a43
Ryan1148 <ryanreynolds1148@gmail.com>
2026-06-04 11:27:14 +0530

    cloud signature

--- api/sign-upload.js
+++ api/sign-upload.js
@@ -3,12 +3,15 @@ import crypto from 'crypto'
 export default function handler(req,res){
     const timestamp = Math.round(Date.now() / 1000)
     const secret = process.env.CLOUDINARY_SECRET_KEY
+    const apiKey = process.env.CLOUDINARY_API_KEY
+    const cloudName = process.env.CLOUDINARY_CLOUD_NAME
 
     const signature = crypto.createHash('sha1').update(`timestamp=${timestamp}${secret}`).digest('hex')
 
     res.status(200).json({
         timestamp,
         signature,
-        secret
-    })
+        apiKey,
+        cloudName
+    }) 
 }
