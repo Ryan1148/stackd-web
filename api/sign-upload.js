48411087d3548405fe2eafbf8abcf57de82b3429
Ryan1148 <ryanreynolds1148@gmail.com>
2026-06-04 14:36:37 +0530

    sig

--- api/sign-upload.js
+++ api/sign-upload.js
@@ -2,7 +2,7 @@ import crypto from 'crypto'
 
 export default function handler(req,res){
     const timestamp = Math.round(Date.now() / 1000)
-    const secret = process.env.CLOUDINARY_SECRET_KEY
+    const secret = process.env.CLOUDINARY_API_SECRET
     const apiKey = process.env.CLOUDINARY_API_KEY
     const cloudName = process.env.CLOUDINARY_CLOUD_NAME
 
