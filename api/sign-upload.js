ecc48a70b8cd2c026521464713b857b04da3a1c5
Ryan1148 <ryanreynolds1148@gmail.com>
2026-06-04 15:59:35 +0530

    aws presigned url

--- api/sign-upload.js
+++ api/sign-upload.js
@@ -1,35 +1,32 @@
-import crypto from 'crypto'
+import {S3Client, PutObjectCommand} from '@aws-sdk/client-s3'
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
 
-export default function handler(req, res) {
-  const timestamp = Math.floor(Date.now() / 1000)
+const s3 = new S3Client({
+    region: process.env.AWS_S3_REGION,
+    credentials:{
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+})
 
-  const publicId = req.query.public_id
+export default async function handler(req,res){
 
-  const secret =
-    process.env.CLOUDINARY_API_SECRET
+    const {fileName,fileType,userID} = req.body
 
-  const apiKey =
-    process.env.CLOUDINARY_API_KEY
+    const key = `Stackd/uploads/${userID}-${fileName}`
+    const cmd = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key:key,
+        ContentType:fileType
+    })
 
-  const stringToSign =
-    `public_id=${publicId}&timestamp=${timestamp}`
+    const signedURL = await getSignedUrl(
+        s3,
+        cmd,
+        {expiresIn:60}
+    )
 
-  console.log('API KEY:', apiKey)
-  console.log('SECRET EXISTS:', !!secret)
-  console.log('STRING:', stringToSign)
-
-  const signature = crypto
-    .createHash('sha1')
-    .update(stringToSign + secret)
-    .digest('hex')
-
-  console.log('SIGNATURE:', signature)
-
-  res.status(200).json({
-    timestamp,
-    signature,
-    apiKey,
-    cloudName:
-      process.env.CLOUDINARY_CLOUD_NAME,
-  })
+    res.status(200).json({
+        signedURL
+    })
 }
