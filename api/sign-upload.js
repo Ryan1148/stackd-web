1bb58bdb2bbce96dd59d8e8197f9463ec9cbc2ce
Ryan1148 <ryanreynolds1148@gmail.com>
2026-06-04 14:46:30 +0530

    signature-last

--- api/sign-upload.js
+++ api/sign-upload.js
@@ -1,20 +1,35 @@
 import crypto from 'crypto'
 
-export default function handler(req,res){
-    const timestamp = Math.round(Date.now() / 1000)
-    const secret = process.env.CLOUDINARY_API_SECRET
-    const apiKey = process.env.CLOUDINARY_API_KEY
-    const cloudName = process.env.CLOUDINARY_CLOUD_NAME
+export default function handler(req, res) {
+  const timestamp = Math.floor(Date.now() / 1000)
 
-    const publicId = req.query.public_id;
-    const stringToSign = `public_id=${publicId}&timestamp=${timestamp}`
+  const publicId = req.query.public_id
 
-    const signature = crypto.createHash('sha1').update(stringToSign + secret).digest('hex')
+  const secret =
+    process.env.CLOUDINARY_API_SECRET
 
-    res.status(200).json({
-        timestamp,
-        signature,
-        apiKey,
-        cloudName
-    }) 
+  const apiKey =
+    process.env.CLOUDINARY_API_KEY
+
+  const stringToSign =
+    `public_id=${publicId}&timestamp=${timestamp}`
+
+  console.log('API KEY:', apiKey)
+  console.log('SECRET EXISTS:', !!secret)
+  console.log('STRING:', stringToSign)
+
+  const signature = crypto
+    .createHash('sha1')
+    .update(stringToSign + secret)
+    .digest('hex')
+
+  console.log('SIGNATURE:', signature)
+
+  res.status(200).json({
+    timestamp,
+    signature,
+    apiKey,
+    cloudName:
+      process.env.CLOUDINARY_CLOUD_NAME,
+  })
 }
