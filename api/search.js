07960c6e0005ffb72370e5854f1cee87321f5eec
Ryan1148 <ryanreynolds1148@gmail.com>
2026-06-07 12:32:56 +0530

    serverless search

---
+++ api/search.js
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import Fuse from 'fuse.js'
+
+const data = JSON.parse(
+    fs.readFileSync('./stackd.exercises.json','utf-8')
+)
+
+const fuse = new Fuse(data,{
+    keys:[
+        {name:'name',weight:0.7},
+        {name:'primaryMuscles',weight:0.3},
+    ],
+    threshold:0.3,
+    ignoreLocation:false,
+    minMatchCharLength:3,
+    includeScore:true
+})
+
+export default function handler(req,res){
+    const q = req.query.q
+    if(!q) return res.json([])
+    const results = fuse.search(q)
+    res.json((results.map(result => result.item)).slice(0,20))
+}
